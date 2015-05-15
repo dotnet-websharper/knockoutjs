d1bd8336887dc138920d9b26d3add6ecf04bd1ba
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-05-15 11:59:49 +0200

    Use if.build to create the nupkg

--- tools/build.fsx
+++ tools/build.fsx
@@ -13,8 +13,10 @@ module C = WebSharper.TypeScript.Compiler
 module U = Utility
 
 open IntelliFactory.Build
+
+let bt = BuildTool().PackageId("WebSharper.Knockout").VersionFrom("WebSharper")
+
 let version, asmVersion =
-    let bt = BuildTool().PackageId("WebSharper.Knockout").VersionFrom("WebSharper")
     let v = PackageVersion.Full.Find(bt)
     let s = match PackageVersion.Current.Find(bt).Suffix with Some s -> "-" + s | None -> ""
     v.ToString() + s, sprintf "%i.%i.0.0" v.Major v.Minor
@@ -63,20 +65,18 @@ match result.CompiledAssembly with
     printfn "Writing %s" out
     File.WriteAllBytes(out, asm.GetBytes())
 
-let (|I|_|) (x: string) =
-    match x with
-    | null | "" -> None
-    | n ->
-        match Int32.TryParse(n) with
-        | true, r -> Some r
-        | _ -> None
-
-let ok =
-    match Environment.GetEnvironmentVariable("NuGetPackageOutputPath") with
-    | null | "" ->
-        U.nuget (sprintf "pack -out build/ -version %s Knockout.nuspec" version)
-    | path ->
-        U.nuget (sprintf "pack -out %s -version %s Knockout.nuspec" path version)
-
-printfn "pack: %b" ok
-if not ok then exit 1 else exit 0
+bt.Solution [
+    bt.NuGet.CreatePackage()
+        .Configure(fun c ->
+            { c with
+                Authors = ["IntelliFactory"]
+                Title = Some "WebSharper.Knockout 3.1.0"
+                LicenseUrl = Some "http://websharper.com/licensing"
+                ProjectUrl = Some "http://websharper.com"
+                Description = "WebSharper bindings for Knockout (3.1.0)"
+                RequiresLicenseAcceptance = true })
+        .AddDependency("WebSharper.TypeScript.Lib")
+        .AddFile("build/WebSharper.Knockout.dll", "lib/net40/WebSharper.Knockout.dll")
+        .AddFile("README.md", "docs/README.md")
+]
+|> bt.Dispatch
