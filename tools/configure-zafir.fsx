59663b2d180ee3e4f00f0e58d28772eb18fe8cc3
Loic Denuziere <loic@denuziere.net>
2017-05-18 10:24:49 +0200

    Use fsi from FSharp.Compiler.Tools
    Avoids issues due to different compiler versions installed on the system.

--- tools/configure-zafir.fsx
+++ tools/configure-zafir.fsx
@@ -1,15 +1,6 @@
 open System
 open System.IO
 
-#load "utility.fsx"
-
-try Directory.Delete(Utility.loc ["packages"], true) with _ -> ()
-let ok =
-    Utility.nuget "install Zafir -pre -o packages -excludeVersion -nocache"
-    && Utility.nuget "install Zafir.TypeScript -pre -o packages -excludeVersion -nocache"
-    && Utility.nuget "install Zafir.TypeScript.Lib -pre -o packages -excludeVersion -nocache"
-    && Utility.nuget "install IntelliFactory.Build -pre -o packages -excludeVersion -nocache"
-
 #I "../packages/NuGet.Core/lib/net40-client"
 #r "NuGet.Core"
 #r "../packages/IntelliFactory.Core/lib/net45/IntelliFactory.Core.dll"
@@ -22,6 +13,3 @@ File.WriteAllText(__SOURCE_DIRECTORY__ + "/version.txt", version)
 
 let tlibVersion = bt.NuGetResolver.FindLatestVersion("Zafir.TypeScript.Lib", true).Value.ToString()
 File.WriteAllText(__SOURCE_DIRECTORY__ + "/tlib-version.txt", tlibVersion)
-
-printfn "configure: %b" ok
-if not ok then exit 1 else exit 0
